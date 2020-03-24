d0a48fb605ba5ee94bedae93e149e41e676e843c
Kevin <kbutchm@gmail.com>
2020-03-24 12:22:34 -0400

    add control flow, delete controlflowtypes project

---
+++ fundamentals/controlflow/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+
+namespace controlflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "if statement";
+            // if statement
+            if (string.IsNullOrEmpty(s))
+            {
+                WriteLine("Emprty string");
+            }
+            else 
+            {
+                WriteLine("string has characters");
+            }
+
+            s = "";
+            if (string.IsNullOrEmpty(s))
+                WriteLine("Empty string");
+            else 
+                WriteLine("string has characters");
+
+
+            // if statement - pattern matching
+            int x = 10;
+            object o = "2";
+            if(o is int i)
+            {
+                WriteLine($"{i} x {x} = {i * x}");
+            }
+            else
+            {
+                WriteLine("o is not an integer");
+            }
+
+            // switch statement
+            var num = (new Random().Next(1,5));
+            switch (num)
+	        {
+                case 1:
+                    WriteLine($"switch statement selcted {num}");
+                    break;
+                case 2:
+                    WriteLine($"switch statement selcted {num}");
+                    break;
+                case 3:
+                    WriteLine($"switch statement selcted {num}");
+                    break;
+                case 4:
+                    WriteLine($"switch statement selcted {num}");
+                    break;
+                case 5:
+                    WriteLine($"switch statement selcted {num}");
+                    break;
+		        default:
+                    break;
+	        }
+
+        }
+    }
+}

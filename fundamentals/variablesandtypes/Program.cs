a3239ed711a53f245d5c4ea792c89cfdd610373f
Kevin <kbutchm@gmail.com>
2020-03-26 09:38:53 -0400

    add variables and types project

---
+++ fundamentals/variablesandtypes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Console;
+
+namespace variablesandtypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int
+            // default = 0, Memory size = 4 bytes, Type = Value Type
+            // .Net Type = System.Int32
+            int x = 5;
+            WriteLine($"integer is {x}");
+
+            // double
+            // default = 0.00, Memory size = 8 bytes, Type = Value Type
+            // .Net Type = System.Double
+            double d = 10.6;
+            WriteLine($"double is {d}");
+
+            // float
+            // default = 0.0f, Memory size = 4 bytes, Type = Value Type
+            // .Net Type = System.Single
+            float f = 9.3f;
+            WriteLine($"float is {f}");
+
+            // float
+            // default = 0.0m, Memory size = 16 bytes, Type = Value Type
+            // .Net Type = System.Decimal
+            decimal m = 6.7m;
+            WriteLine($"decimal is {m}");
+
+            // char
+            // default = \0, Memory size = 2 bytes, Type = Value Type
+            char letter = 'a';
+            WriteLine($"char is {letter}");
+
+            // string
+            // default = null, Memory size = (2 bytes) * (length of string), Type = Reference Type
+            string str = "hello";
+            WriteLine($"string is {str}");
+
+            // bool
+            // default = null, Memory size = (2 bytes) * (length of string), Type = Reference Type
+            Random random = new Random();
+            bool isTrue = Convert.ToBoolean(random.Next(0,2));
+            if(isTrue)
+            {
+                WriteLine($"{isTrue} selected");
+            }
+            else 
+            {
+                WriteLine($"{isTrue} is selected");
+            }
+                
+        }
+    }
+}
